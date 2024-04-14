5ec3fcdff7c7342d49da1ba3df41fb2335211f7a
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-04-14 20:43:26 +0500

    create login page

--- app/src/main/java/com/smd/surmaiya/LoginActivity.kt
+++ app/src/main/java/com/smd/surmaiya/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.smd.surmaiya
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
