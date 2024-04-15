d7d4a8ed64bdaa9affc75e61bceae796368562e0
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-04-15 23:12:36 +0500

    create profile and artist page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation ("com.google.android.material:material:1.4.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 }
