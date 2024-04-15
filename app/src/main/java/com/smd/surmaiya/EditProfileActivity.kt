9639864e809099f290b326de28e067a94063c122
Moaz Farrukh <maazfarrukh@gmail.com>
2024-04-15 21:18:56 +0500

    add equalizer and settings

---
+++ app/src/main/java/com/smd/surmaiya/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.smd.surmaiya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfileActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
