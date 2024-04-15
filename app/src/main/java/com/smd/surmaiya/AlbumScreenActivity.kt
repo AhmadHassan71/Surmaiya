d14f87b826fbf5d297eae8453f626fdb1ae55307
Moaz Farrukh <maazfarrukh@gmail.com>
2024-04-15 17:15:07 +0500

    add album and song add screens

---
+++ app/src/main/java/com/smd/surmaiya/AlbumScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.smd.surmaiya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AlbumScreenActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_screen)
+    }
+}
