c1985c779651e46f943524f6c25a2685b1bf5888
Moaz Farrukh <maazfarrukh@gmail.com>
2024-04-15 15:26:33 +0500

    add search and playlist screens

---
+++ app/src/main/java/com/smd/surmaiya/LibraryActivity.kt
@@ -0,0 +1,16 @@
+package com.smd.surmaiya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+
+
+    }
+}
+    
